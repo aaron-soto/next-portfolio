527121e80be8d1f00da805eb71c5f60757693673
aaron-soto <aaron.m.soto1@gmail.com>
2024-06-05 10:55:54 -0700

    fixing vercel errors

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -1,18 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-const base = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
+const base = Buffer.from(
+  `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+).toString("base64");
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
   const response = await fetch(TOKEN_ENDPOINT, {
-    method: 'POST',
+    method: "POST",
     headers: {
       Authorization: `Basic ${base}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams({
-      grant_type: 'refresh_token',
+      grant_type: "refresh_token",
       refresh_token: process.env.SPOTIFY_REFRESH_TOKEN!,
     }).toString(),
   });
@@ -20,7 +22,7 @@ const getAccessToken = async () => {
   return response.json();
 };
 
-export const getNowPlaying = async () => {
+const getNowPlaying = async () => {
   const { access_token } = await getAccessToken();
 
   return fetch(NOW_PLAYING_ENDPOINT, {
@@ -39,7 +41,7 @@ export async function GET(req: NextRequest) {
   if (
     response.status === 204 ||
     response.status > 400 ||
-    responseData.currently_playing_type !== 'track'
+    responseData.currently_playing_type !== "track"
   ) {
     return NextResponse.json({ isPlaying: false }, { status: 200 });
   }
@@ -48,7 +50,9 @@ export async function GET(req: NextRequest) {
     isPlaying: responseData.is_playing,
     title: responseData.item.name,
     album: responseData.item.album.name,
-    artist: responseData.item.album.artists.map((artist: any) => artist.name).join(', '),
+    artist: responseData.item.album.artists
+      .map((artist: any) => artist.name)
+      .join(", "),
     albumImageUrl: responseData.item.album.images[0].url,
     songUrl: responseData.item.external_urls.spotify,
   };
